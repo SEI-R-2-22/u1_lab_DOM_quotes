3e4998dc2c3466a7fe67cd55aefb4af9446de6bc
Adam Honore <61853978+ahonore42@users.noreply.github.com>
2021-01-16 19:30:27 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// TODO: [1] Select the <body>
+
+// TODO: [2] Select the <header>
+
+// TODO: [3] Select all of the elements with class "quote"
+
+// TODO: [4] Select all of the subject elements
+
+// TODO: [5] Select the navigation links
+
+// TODO: [6] Select all the quotes in the "life" section.
+
+// TODO: [7] Using relative selection, select the <p> element containing the second Mark Twain quote
+
+// TODO: [8] Do the same using a query selector
+
+// TODO: [9] Select the 'Quotes About Motivation' heading
+
+// TODO: [10] -  Add a class 'simplicity' to the first subject.
+
+
+// BONUS
+// TODO: [11] Create an element with the text of your favorite quote.
+
+// TODO: [12] Add the element that you created in the previous step to the layout by appending it to a corresponding subject section.
+
+// TODO: [13] Add the author's last name, as a class name, to one of the quotes.
